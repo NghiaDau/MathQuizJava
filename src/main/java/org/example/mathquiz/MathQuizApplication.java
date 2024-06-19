b01c67d60bc24832ca0a6050e637a13f89b5c3f5
VanNghia <nghiadauvan@gmail.com>
2024-06-19 15:15:59 +0700

    load project

---
+++ src/main/java/org/example/mathquiz/MathQuizApplication.java
@@ -0,0 +1,13 @@
+package org.example.mathquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathQuizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/mathquiz/MathQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mathquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
