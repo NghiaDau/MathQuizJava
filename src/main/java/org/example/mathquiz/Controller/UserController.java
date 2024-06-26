2507d7978fecdb2993c0499a9a151c7cdbdcd98c
VanNghia <nghiadauvan@gmail.com>
2024-06-27 00:09:52 +0700

    update user

--- src/main/java/org/example/mathquiz/Controller/UserController.java
+++ src/main/java/org/example/mathquiz/Controller/UserController.java
@@ -1,5 +1,6 @@
 package org.example.mathquiz.Controller;
 
+import org.example.mathquiz.Entities.User;
 import org.example.mathquiz.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,14 +8,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("user")
 public class UserController {
     @Autowired
     private UserService userService;
-    @GetMapping()
+    @GetMapping("")
     public String displayAllUser(Model model){
-        model.addAttribute("users",userService.getAllUser());
+
+        List<User> users = userService.getAllUser();
+        System.out.println(users);
+        model.addAttribute("list_user",users);
         return "user/index";
     }
 }
--- src/main/java/org/example/mathquiz/Service/UserService.java
+++ src/main/java/org/example/mathquiz/Service/UserService.java
@@ -4,15 +4,16 @@ import org.example.mathquiz.Entities.User;
 import org.example.mathquiz.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
+
 
 import java.util.List;
 
 @Service
 public class UserService {
     @Autowired
-    private UserRepository userRepository;
+    private  UserRepository userRepository;
     public List<User> getAllUser(){
-        return userRepository.findAll();
+        List<User> users = userRepository.findAll();
+        return users;
     }
 }
