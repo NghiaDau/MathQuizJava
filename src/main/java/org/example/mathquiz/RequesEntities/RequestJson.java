9e436619514c66d0c1f6833cc1eca7a0125a9c72
vangggggggg <95358830+vangggggggg@users.noreply.github.com>
2024-07-05 00:47:26 +0700

    AC QUY 333

--- src/main/java/org/example/mathquiz/RequesEntities/RequestJson.java
+++ src/main/java/org/example/mathquiz/RequesEntities/RequestJson.java
@@ -12,4 +12,5 @@ import lombok.Setter;
 public class RequestJson {
     private String name;
     private String id;
+    private String status;
 }
--- src/main/resources/static/js/menu.js
+++ src/main/resources/static/js/menu.js
@@ -1,11 +1,33 @@
-fetch("http://localhost:8080/api/menu")
-    .then(function (data) {
-        return data.json();
-    }).then(function (menus) {
-    const menuContainer = document.getElementById("menu");
-    displayMenuIds(menus, menuContainer);
-    console.log(menus);
-});
+function dataMenus() {
+    return fetch("http://localhost:8080/api/menu")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (menus) {
+            localStorage.setItem('cachedMenus', JSON.stringify(menus));
+            return menus;
+        })
+        .catch(function (error) {
+            console.error('Error fetching menus:', error);
+        });
+}
+
+function getCachedMenus() {
+    const cachedMenus = localStorage.getItem('cachedMenus');
+    return cachedMenus ? JSON.parse(cachedMenus) : null;
+}
+
+let cachedMenus = getCachedMenus();
+const menuContainer = document.getElementById("menu");
+if (cachedMenus) {
+    console.log('Using cached menus:', cachedMenus);
+    displayMenuIds(cachedMenus, menuContainer)
+} else {
+    dataMenus().then(function (menus) {
+        console.log('Fetched menus from API:', menus);
+        displayMenuIds(cachedMenus, menuContainer)
+    });
+}
 
 // function displayMenuIds(data, parentElement) {
 //     data.forEach(item => {
@@ -32,9 +54,9 @@ fetch("http://localhost:8080/api/menu")
 // }
 function displayMenuIds(data, parentElement) {
     data.forEach(item => {
-        if(item.level >= 3){
+        if (item.level >= 3) {
             parentElement.style.top = '0%';
-            parentElement.style.left='100px';
+            parentElement.style.left = '100px';
             parentElement.style.position = 'absolute';
         }
         const li = document.createElement('li');
